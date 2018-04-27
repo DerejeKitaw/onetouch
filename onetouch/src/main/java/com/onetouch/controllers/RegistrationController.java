3b204d3c6feaba88b471729433e90a12cb1d6a8d
SantoshKorada <santuk95@gmail.com>
2018-04-27 23:29:29 +0530

    added logback.xml
    logging functionality

--- onetouch/src/main/java/com/onetouch/controllers/RegistrationController.java
+++ onetouch/src/main/java/com/onetouch/controllers/RegistrationController.java
@@ -10,8 +10,14 @@ import com.onetouch.models.LoginModel;
 import com.onetouch.models.RegistrationModel;
 import com.onetouch.services.RegisterService;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
 @RestController
 @RequestMapping(value="registration")
+@Slf4j
 public class RegistrationController {
 
 	@Autowired
@@ -20,8 +26,11 @@ public class RegistrationController {
 	@RequestMapping(value="register",method=RequestMethod.POST,consumes = { "application/json" }, produces="application/json")
 	public String registerUser(@RequestBody RegistrationModel registrationModel )
 	{
+		log.info("Inside Registration Controller");
+		log.info("RegistrationModel :: "+registrationModel.toString());
 		System.out.println("RegistrationModel :: "+registrationModel.toString());
 		String registered =registerService.registerUser(registrationModel);
+		log.info("Registerd :: "+registered);
 		System.out.println("Registerd :: "+registered);
 		return registered;
 	}
@@ -29,6 +38,7 @@ public class RegistrationController {
 	@RequestMapping(value="validLogin",method=RequestMethod.POST,consumes = { "application/json" }, produces="application/json")
 	public RegistrationModel validLogin(@RequestBody LoginModel loginModel)
 	{
+		log.info("LoginModel :: "+loginModel.toString());
 		System.out.println("LoginModel :: "+loginModel.toString());
 		return registerService.validateLogin(loginModel);
 	}
--- onetouch/src/main/java/com/onetouch/daoImpl/RegistrationDaoImpl.java
+++ onetouch/src/main/java/com/onetouch/daoImpl/RegistrationDaoImpl.java
@@ -12,7 +12,10 @@ import com.onetouch.models.RegistrationModel;
 import com.onetouch.utilities.ExtractingProperty;
 import com.onetouch.utilities.RegisterUtility;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Repository
+@Slf4j
 public class RegistrationDaoImpl implements RegistrationDao {
 
 	@Autowired
@@ -25,7 +28,9 @@ public class RegistrationDaoImpl implements RegistrationDao {
 	public String registerUser(RegistrationModel registrationModel) {
 		// TODO Auto-generated method stub
 		String registerUserQuery = ExtractingProperty.getProperty("register", "");
+		log.info("registerUserQuery :: " + registerUserQuery);
 		System.out.println("registerUserQuery :: " + registerUserQuery);
+		log.info("password encryotion ::" + RegisterUtility.encrypt(registrationModel.getPassword()));
 		System.out.println("password encryotion ::" + RegisterUtility.encrypt(registrationModel.getPassword()));
 		int inserted = jdbcTemplate.update(registerUserQuery,
 				new Object[] { registrationModel.getFirst_name(), registrationModel.getLast_name(),
@@ -39,13 +44,16 @@ public class RegistrationDaoImpl implements RegistrationDao {
 	public RegistrationModel validateLogin(LoginModel loginModel) {
 		// TODO Auto-generated method stub
 		String loginQuery = ExtractingProperty.getProperty("validLogin", "");
+		log.info("loginQuery :: " + loginQuery);
 		System.out.println("loginQuery :: " + loginQuery);
+		log.info("password encryotion ::" + RegisterUtility.encrypt(loginModel.getPassword()));
 		System.out.println("password encryotion ::" + RegisterUtility.encrypt(loginModel.getPassword()));
 		RegistrationModel registrationModel = jdbcTemplate.queryForObject(loginQuery,
 				new Object[] { loginModel.getEmail_id(), loginModel.getPhone_no(),
 						RegisterUtility.encrypt(loginModel.getPassword()) },
 				new BeanPropertyRowMapper<RegistrationModel>(RegistrationModel.class));
 		System.out.println("extracted user info ::" + registrationModel.toString());
+		log.info("extracted user info ::" + registrationModel.toString());
 		return registrationModel;
 	}
 
